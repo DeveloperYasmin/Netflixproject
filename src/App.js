3cc74699925e661ecb693c6234217dff53ab9061
DeveloperYasmin <banuyasmin96@gmail.com>
2023-12-14 08:11:53 +0530

    Updated

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="text-2xl font-bold font-serif text-red-900">
+      Namaste Everyone,Lets build Netflix-GPT... Make Happen Girl Shock Everyone 
+    </div>
+  );
+}
+
+export default App;
