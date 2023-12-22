15de1252e4f0cb37b1a2dac2a5aa6b7a1048c214
DeveloperYasmin <banuyasmin96@gmail.com>
2023-12-22 20:14:04 +0530

    Updated

--- src/hooks/usePopularMovies.js
+++ src/hooks/usePopularMovies.js
@@ -1,4 +1,4 @@
-import { useDispatch } from "react-redux"
+import { useDispatch, useSelector } from "react-redux"
 import { API_OPTIONS } from '../utils/constants'
 import { addpopularmovies } from '../utils/movieSlice'
 import { useEffect } from "react"
--- src/hooks/useUpcoming.js
+++ src/hooks/useUpcoming.js
@@ -1,4 +1,4 @@
-import { useDispatch } from "react-redux"
+import { useDispatch, useSelector } from "react-redux"
 import { API_OPTIONS } from '../utils/constants'
 import { addupcoming } from '../utils/movieSlice'
 import { useEffect } from "react"
