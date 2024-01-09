daaf578c75f76adcd2be5699663d01978c45fa6b
DeveloperYasmin <banuyasmin96@gmail.com>
2024-01-09 18:46:42 +0530

    Update

--- src/App.js
+++ src/App.js
@@ -1,13 +1,39 @@
-import Body from "./components/Body";
+import ReactDOM from "react-dom/client";
+import Login from "./components/Login";
+
 import {Provider} from 'react-redux'
 import appStore from "./utils/appStore";
+import Header from "./components/Header";
+import {RouterProvider,Outlet, createHashRouter } from "react-router-dom";
+import Browse from "./components/Browse";
 
 function App() {
-  return (  
-     <Provider store={appStore}>
-          <Body/>
-      </Provider> 
-  );
+  const AppLayout =() =>{
+    return (
+      <Provider store={appStore}>
+     <div>
+       <Header/>
+       <Outlet/>
+           </div>
+           </Provider>
+    )
+  }
+  const appRouter=createHashRouter([{
+    path:"/",
+    element:<AppLayout/>,
+    children:[
+     {
+      path:"/",
+      element:<Login/>,
+     },
+     {
+      path:"/Browse",
+      element:<Browse/>,
+     }
+    ]},])
+  ReactDOM.createRoot(document.getElementById("root")).render(<RouterProvider router={appRouter}/>)  
+
+  
 }
 
 export default App;
--- src/components/Browse.js
+++ src/components/Browse.js
@@ -18,7 +18,7 @@ const Browse = () => {
   
   return (
     <div>
-    <Header/>
+    
     {showGptSearch?
     (<GptSearch/>):
     <>
